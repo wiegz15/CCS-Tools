1662ba1ba63e3f8ddf87df8a28105963d14954b3
wiegz15 <alex.wiegel@wiegz.net>
2024-05-16 09:45:33 -0400

    DuplicateSPN

--- AD/Scripts/Duplicate_SPN.ps1
+++ AD/Scripts/Duplicate_SPN.ps1
@@ -10,6 +10,38 @@ $computerSPNs = Get-ADComputer -Filter {ServicePrincipalName -ne $null} -Propert
 # Combine results into a single array
 $allSPNs = $userSPNs + $computerSPNs
 
+# Flatten the SPNs into a single list with account information
+$spnList = @()
+foreach ($account in $allSPNs) {
+    foreach ($spn in $account.ServicePrincipalName) {
+        $spnList += [PSCustomObject]@{
+            AccountName = $account.SamAccountName
+            SPN         = $spn
+        }
+    }
+}
+
+# Find duplicate SPNs
+$duplicateSPNs = $spnList | Group-Object SPN | Where-Object { $_.Count -gt 1 }
+
+# Prepare output
+$output = @()
+if ($duplicateSPNs.Count -eq 0) {
+    $output += [PSCustomObject]@{
+        AccountName = "None"
+        SPN         = "No duplicate SPNs found"
+    }
+} else {
+    foreach ($group in $duplicateSPNs) {
+        foreach ($item in $group.Group) {
+            $output += [PSCustomObject]@{
+                AccountName = $item.AccountName
+                SPN         = $item.SPN
+            }
+        }
+    }
+}
+
 # Export to Excel
 $excelPath = Join-Path -Path $reportsDir -ChildPath "AD_Output.xlsx"
-$allSPNs | Export-Excel -Path $excelPath -WorksheetName "SPNs" -AutoSize -TableName "SPNs" -TableStyle Medium22 -Append
+$output | Export-Excel -Path $excelPath -WorksheetName "Duplicate SPNs" -AutoSize -TableName "DuplicateSPNs" -TableStyle Medium22 -Append
