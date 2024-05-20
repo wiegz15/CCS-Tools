ea2d3af7048f4419c6f0567ea97097184b8da914
wiegz15 <alex.wiegel@wiegz.net>
2024-05-20 15:25:04 -0400

    StaleServers

--- AD/Scripts/Stale_Accounts.ps1
+++ AD/Scripts/Stale_Accounts.ps1
@@ -4,13 +4,9 @@ Import-Module ImportExcel
 # Define the threshold for inactivity (e.g., 90 days)
 $thresholdDate = (Get-Date).AddDays(-90)
 
-# Get stale user accounts
-$staleUsers = Get-ADUser -Filter {LastLogonDate -lt $thresholdDate -and Enabled -eq $true} -Properties LastLogonDate | Select-Object Name, SamAccountName, LastLogonDate
-
-# Get stale computer accounts
-$staleComputers = Get-ADComputer -Filter {LastLogonDate -lt $thresholdDate -and Enabled -eq $true} -Properties LastLogonDate | Select-Object Name, SamAccountName, LastLogonDate
+# Get stale server accounts
+$staleServers = Get-ADComputer -Filter {LastLogonDate -lt $thresholdDate -and Enabled -eq $true -and OperatingSystem -like '*Server*'} -Properties LastLogonDate | Select-Object Name, SamAccountName, LastLogonDate
 
 # Export to Excel
 $excelPath = Join-Path -Path $reportsDir -ChildPath "AD_Output.xlsx"
-$staleUsers | Export-Excel -Path $excelPath -WorksheetName "Stale Users" -AutoSize -TableName "StaleUsers" -TableStyle Medium18 -Append
-$staleComputers | Export-Excel -Path $excelPath -WorksheetName "Stale Computers" -AutoSize -TableName "StaleComputers" -TableStyle Medium19 -Append
+$staleServers | Export-Excel -Path $excelPath -WorksheetName "Stale Servers" -AutoSize -TableName "StaleServers" -TableStyle Medium19 -Append
