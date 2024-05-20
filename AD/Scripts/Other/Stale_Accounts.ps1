e262c99dc2cd8e1450e940685d04965bb912a27e
wiegz15 <alex.wiegel@wiegz.net>
2024-05-20 16:05:03 -0400

    Moved Around

---
+++ AD/Scripts/Other/Stale_Accounts.ps1
@@ -0,0 +1,17 @@
+Import-Module ActiveDirectory
+Import-Module ImportExcel
+
+# Define the threshold for inactivity (e.g., 90 days)
+$thresholdDate = (Get-Date).AddDays(-90)
+
+# Get stale user accounts
+$staleUsers = Get-ADUser -Filter {LastLogonDate -lt $thresholdDate -and Enabled -eq $true} -Properties LastLogonDate | Select-Object Name, SamAccountName, LastLogonDate
+
+# Get stale computer accounts
+$staleComputers = Get-ADComputer -Filter {LastLogonDate -lt $thresholdDate -and Enabled -eq $true} -Properties LastLogonDate | Select-Object Name, SamAccountName, LastLogonDate
+
+# Export to Excel
+$excelPath = Join-Path -Path $reportsDir -ChildPath "Stale_Devices.xlsx"
+$staleUsers | Export-Excel -Path $excelPath -WorksheetName "Stale Users" -AutoSize -TableName "StaleUsers" -TableStyle Medium18 -Append
+$staleComputers | Export-Excel -Path $excelPath -WorksheetName "Stale Computers" -AutoSize -TableName "StaleComputers" -TableStyle Medium19 -Append
+can I have this just look at StaleServers instead of all computers
