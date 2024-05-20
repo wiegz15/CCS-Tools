207e8667437993b3a2c076196011f4f562b83c86
wiegz15 <alex.wiegel@wiegz.net>
2024-05-20 13:12:03 -0400

    DNS Update

--- AD/Scripts/DNS_Check.ps1
+++ AD/Scripts/DNS_Check.ps1
@@ -14,15 +14,24 @@ foreach ($dc in $domainControllers) {
     $dnsForwarders = Get-DnsServerForwarder -ComputerName $dc.HostName -ErrorAction SilentlyContinue | Select-Object -ExpandProperty IPAddress | ForEach-Object { $_.IPAddressToString }    
     $dnsForwarderString = if ($dnsForwarders) { $dnsForwarders -join ', ' } else { "No forwarders configured" }
 
-    $dnsSettings += [PSCustomObject]@{
-        DCName           = $dc.HostName
-        Scavenging       = $zonesWithScavenging.Count -gt 0
-        Forwarders       = $dnsForwarderString
-        ZonesScavenging  = $zonesWithScavenging.Count
-        RootHints        = (Get-DnsServerRootHint -ComputerName $dc.HostName).Count
+    # Fetch aging details for each zone
+    $zoneAgingDetails = $dnsZones | ForEach-Object {
+        Get-DnsServerZoneAging -ComputerName $dc.HostName -Name $_.ZoneName
+    } | Select-Object ZoneName, AvailForScavengeTime, @{Name='ScavengingEnabled'; Expression={$_.AgingEnabled}}
+
+    # Combine details in custom object
+    foreach ($zoneDetail in $zoneAgingDetails) {
+        $dnsSettings += [PSCustomObject]@{
+            DCName              = $dc.HostName
+            ZoneName            = $zoneDetail.ZoneName
+            Scavenging          = $zoneDetail.ScavengingEnabled
+            AvailForScavengeTime = $zoneDetail.AvailForScavengeTime
+            Forwarders          = $dnsForwarderString
+            RootHints           = (Get-DnsServerRootHint -ComputerName $dc.HostName).Count
+        }
     }
 }
 
-# Export to Excel
+# Optionally, export to Excel
 $excelPath = Join-Path -Path $reportsDir -ChildPath "AD_Output.xlsx"
 $dnsSettings | Export-Excel -Path $excelPath -WorksheetName "DNS Settings" -AutoSize -TableName "DNSSettings" -TableStyle Medium10 -Append
