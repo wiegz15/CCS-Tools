8a6d860be1dac809ad0f171b37eb2d3ca075a2ba
wiegz15 <alex.wiegel@wiegz.net>
2024-05-21 13:21:52 -0400

    Launch.bat fix

--- Launch.bat
+++ Launch.bat
@@ -1,3 +1,3 @@
 @echo off
 cd %~dp0
-Start /min powershell.exe -ExecutionPolicy Bypass -WindowStyle Hidden -File ".\CCS-Tools\CCS-Tools-Launcher.ps1"
+Start /min powershell.exe -ExecutionPolicy Bypass -WindowStyle Hidden -File ".\CCS-Tools-Launcher.ps1"
