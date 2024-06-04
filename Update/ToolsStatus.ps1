5bd9b862c2e5cb4ba1f1ca9c2830de93cec20290
wiegz15 <alex.wiegel@wiegz.net>
2024-06-04 13:51:54 -0400

    ToolsStatus

--- Update/ToolsStatus.ps1
+++ Update/ToolsStatus.ps1
@@ -1,9 +1,7 @@
 $featuresToCheck = 'RSAT-Role-Tools', 'RSAT-AD-Tools', 'RSAT-AD-PowerShell', 'RSAT-ADDS', 'RSAT-ADDS-Tools', 'RSAT-ADLDS', 'RSAT-RDS-Tools', 'RSAT-DNS-Server'
-$modulesToCheck = 'ImportExcel', 'VMware.PowerCLI'
+$modulesToCheck = 'ImportExcel', 'VMware.PowerCLI', 'GroupPolicy', 'ActiveDirectory', 'DNSServer'
 
 $installedFeatures = Get-WindowsFeature | Where-Object {$_.Installed -eq $true} | Select-Object -ExpandProperty Name
-$installedModules = Get-Module -ListAvailable | Select-Object -ExpandProperty Name
-
 $status = @()
 
 foreach ($feature in $featuresToCheck) {
@@ -15,7 +13,8 @@ foreach ($feature in $featuresToCheck) {
 }
 
 foreach ($module in $modulesToCheck) {
-    if ($installedModules -contains $module) {
+    $moduleCheck = Get-Module -ListAvailable -Name $module
+    if ($null -ne $moduleCheck) {
         $status += "${module}: Installed"
     } else {
         $status += "${module}: Not Installed"
