d6b31e9783a9cc84c1db3621dabc0bc15f445db0
wiegz15 <alex.wiegel@wiegz.net>
2024-06-22 12:37:10 -0400

    VMware_Assessment Updates

--- Vmware/Scripts/Assessments/VMware_Assessment.ps1
+++ Vmware/Scripts/Assessments/VMware_Assessment.ps1
@@ -1,9 +1,19 @@
 Import-Module ImportExcel
 
+# Determine the path to the vSphereOvercommit module
+$scriptDir = Split-Path -Parent $PSCommandPath
+$modulePath = Join-Path -Path (Split-Path -Parent (Split-Path -Parent $scriptDir)) -ChildPath "bin\vSphereOvercommit.psm1"
+
+# Import the vSphereOvercommit module
+Import-Module $modulePath
+
 # Get all the clusters
 $clusters = Get-Cluster
 $clusterData = @()
 $hostData = @()
+$averageData = @()
+$vmData = @()
+$datastoreData = @()
 
 # Loop through the clusters and extract the required data
 foreach ($cluster in $clusters) {
@@ -13,10 +23,10 @@ foreach ($cluster in $clusters) {
      
     # Calculate cluster metrics
     $ClusterPoweredOnvCPUs = (Get-VM -Location $cluster | Where-Object {$_.PowerState -eq "PoweredOn" }).NumCpu | Measure-Object -Sum
-    $ClusterCPUCores = ($vmhosts.NumCpu | Measure-Object -Sum).Sum
+    $ClusterCPUCores = ($vmhosts | Measure-Object -Property NumCpu -Sum).Sum
 
     $ClusterPoweredOnvRAM = (Get-VM -Location $cluster | Where-Object {$_.PowerState -eq "PoweredOn" }).MemoryGB | Measure-Object -Sum
-    $ClusterPhysRAM = ($vmhosts.MemoryTotalGB | Measure-Object -Sum).Sum
+    $ClusterPhysRAM = ($vmhosts | Measure-Object -Property MemoryTotalGB -Sum).Sum
 
     $oneesxi = if ($num_esxis) { $num_esxis - 1 } else { $null }
     $coresPerESXi = if ($num_esxis) { $ClusterCPUCores / $num_esxis } else { $null }
@@ -27,6 +37,19 @@ foreach ($cluster in $clusters) {
     $ramPerClusterMinusOne = $ramPerESXi * $oneesxi
     $TotalRAMPerClusterMinusOne = [Math]::Round($ramPerClusterMinusOne)
 
+    # Retrieve average CPU and RAM usage over the past day
+    $avgCPUUsage = Get-Stat -Entity $vmhosts -Stat "cpu.usage.average" -Start (Get-Date).AddDays(-7) -IntervalMins 30 | Measure-Object -Property Value -Average
+    $avgRAMUsage = Get-Stat -Entity $vmhosts -Stat "mem.usage.average" -Start (Get-Date).AddDays(-7) -IntervalMins 30 | Measure-Object -Property Value -Average
+
+    $avgCPUSum = $avgCPUUsage.Average * $ClusterCPUCores / 100
+    $avgRAMSum = $avgRAMUsage.Average * $ClusterPhysRAM / 100
+
+    $avgCoresPerClusterMinusOne = $coresPerESXi * $oneesxi
+    $TotalAvgCoresPerClusterMinusOne = [Math]::Round($avgCoresPerClusterMinusOne)
+
+    $avgRAMPerClusterMinusOne = $ramPerESXi * $oneesxi
+    $TotalAvgRAMPerClusterMinusOne = [Math]::Round($avgRAMPerClusterMinusOne)
+
     # Create an ordered hashtable for each cluster
     $clusterProperty = [ordered]@{
         "vCenter" = $vcenter
@@ -36,20 +59,37 @@ foreach ($cluster in $clusters) {
         "vCPU" = (Get-VM -Location $cluster | Measure-Object -Property NumCpu -Sum).Sum
         "PoweredOn vCPUs" = $ClusterPoweredOnvCPUs.Sum
         "PoweredOn VMs" = (Get-VM -Location $cluster | Where-Object { $_.PowerState -eq "PoweredOn" }).Count
-        "vCPU:pCPU Ratio" = [Math]::Round($ClusterPoweredOnvCPUs.Sum / $ClusterCPUCores, 3)
-        "vCPU:pCPU Ratio with one ESXi failed" = [Math]::Round($ClusterPoweredOnvCPUs.Sum / $TotalCoresPerClusterMinusOne, 3)
-        "CPU Overcommit (%)" = [Math]::Round(100 * ($ClusterPoweredOnvCPUs.Sum / $ClusterCPUCores), 3)
+        "vCPU:pCPU Ratio" = if ($ClusterCPUCores -ne 0) { [Math]::Round($ClusterPoweredOnvCPUs.Sum / $ClusterCPUCores, 3) } else { 0 }
+        "vCPU:pCPU Ratio with one ESXi failed" = if ($TotalCoresPerClusterMinusOne -ne 0) { [Math]::Round($ClusterPoweredOnvCPUs.Sum / $TotalCoresPerClusterMinusOne, 3) } else { 0 }
+        "CPU Overcommit (%)" = if ($ClusterCPUCores -ne 0) { [Math]::Round(100 * (($ClusterPoweredOnvCPUs.Sum - $ClusterCPUCores) / $ClusterCPUCores), 2) } else { 0 }
         "pRAM(GB)" = $ClusterPhysRAM
         "vRAM(GB)" = [Math]::Round((Get-VM -Location $cluster | Measure-Object -Property MemoryGB -Sum).Sum, 2)
         "PoweredOn vRAM (GB)" = $ClusterPoweredOnvRAM.Sum
-        "vRAM:pRAM Ratio" = [Math]::Round($ClusterPoweredOnvRAM.Sum / $ClusterPhysRAM, 3)
-        "vRAM:pRAM Ratio with one ESXi failed" = [Math]::Round($ClusterPoweredOnvRAM.Sum / $TotalRAMPerClusterMinusOne, 3)
-        "RAM Overcommit (%)" = [Math]::Round(100 * ($ClusterPoweredOnvRAM.Sum / $ClusterPhysRAM), 2)
+        "vRAM:pRAM Ratio" = if ($ClusterPhysRAM -ne 0) { [Math]::Round($ClusterPoweredOnvRAM.Sum / $ClusterPhysRAM, 3) } else { 0 }
+        "vRAM:pRAM Ratio with one ESXi failed" = if ($TotalRAMPerClusterMinusOne -ne 0) { [Math]::Round($ClusterPoweredOnvRAM.Sum / $TotalRAMPerClusterMinusOne, 3) } else { 0 }
+        "RAM Overcommit (%)" = if ($ClusterPhysRAM -ne 0) { [Math]::Round(100 * (($ClusterPoweredOnvRAM.Sum - $ClusterPhysRAM) / $ClusterPhysRAM), 2) } else { 0 }
+        "Avg CPU Usage (%)" = [Math]::Round($avgCPUUsage.Average, 2)
+        "Avg RAM Usage (%)" = [Math]::Round($avgRAMUsage.Average, 2)
     }
-    
 
     # Add the cluster data to the array
     $clusterData += New-Object -TypeName psobject -Property $clusterProperty
+
+    # Create an ordered hashtable for average data
+    $averageProperty = [ordered]@{
+        "vCenter" = $vcenter
+        "Cluster Name" = $cluster.Name
+        "Avg CPU Usage (%)" = [Math]::Round($avgCPUUsage.Average, 2)
+        "Avg RAM Usage (%)" = [Math]::Round($avgRAMUsage.Average, 2)
+        "Avg vCPU:pCPU Ratio" = if ($ClusterCPUCores -ne 0) { [Math]::Round($avgCPUSum / $ClusterCPUCores, 3) } else { 0 }
+        "Avg vCPU:pCPU Ratio with one ESXi failed" = if ($TotalAvgCoresPerClusterMinusOne -ne 0) { [Math]::Round($avgCPUSum / $TotalAvgCoresPerClusterMinusOne, 3) } else { 0 }
+        "Avg RAM Overcommit (%)" = if ($ClusterPhysRAM -ne 0) { [Math]::Round(100 * (($avgRAMSum - $ClusterPhysRAM) / $ClusterPhysRAM), 2) } else { 0 }
+        "Avg vRAM:pRAM Ratio" = if ($ClusterPhysRAM -ne 0) { [Math]::Round($avgRAMSum / $ClusterPhysRAM, 3) } else { 0 }
+        "Avg vRAM:pRAM Ratio with one ESXi failed" = if ($TotalAvgRAMPerClusterMinusOne -ne 0) { [Math]::Round($avgRAMSum / $TotalAvgRAMPerClusterMinusOne, 3) } else { 0 }
+    }
+
+    # Add the average data to the array
+    $averageData += New-Object -TypeName psobject -Property $averageProperty
 
     # Loop through each host in the cluster to gather host data
     foreach ($vmHost in $vmhosts) {
@@ -73,13 +113,49 @@ foreach ($cluster in $clusters) {
             FirmwareVersion = $firmwareVersion
         }
     }
+
+    # Gather powered-on VMs data
+    $poweredOnVMs = Get-VM -Location $cluster | Where-Object { $_.PowerState -eq "PoweredOn" }
+    foreach ($vm in $poweredOnVMs) {
+        # Calculate average CPU and RAM usage over the last 7 days
+        $avgVMCPUUsage = Get-Stat -Entity $vm -Stat "cpu.usage.average" -Start (Get-Date).AddDays(-7) -IntervalMins 30 | Measure-Object -Property Value -Average
+        $avgVMRAMUsage = Get-Stat -Entity $vm -Stat "mem.usage.average" -Start (Get-Date).AddDays(-7) -IntervalMins 30 | Measure-Object -Property Value -Average
+
+        # Add the VM data to the array
+        $vmData += [PSCustomObject]@{
+            VMName = $vm.Name
+            vCPU = $vm.NumCpu
+            RAMGB = $vm.MemoryGB
+            StorageGB = [Math]::Round(($vm.HardDisks | Measure-Object -Property CapacityGB -Sum).Sum, 2)
+            AvgCPUUsage = if ($avgVMCPUUsage.Average) { [Math]::Round($avgVMCPUUsage.Average, 2) } else { 0 }
+            AvgRAMUsage = if ($avgVMRAMUsage.Average) { [Math]::Round($avgVMRAMUsage.Average, 2) } else { 0 }
+        }
+    }
+
+    # Gather datastore data
+    $datastores = Get-Cluster $cluster | Get-Datastore | Where-Object { $_.Name -notlike "*ISO*" -and $_.Name -notlike "*loca*" } | Sort-Object -Property FreeSpaceGB -Descending
+    foreach ($datastore in $datastores) {
+        # Add the datastore data to the array
+        $datastoreData += [PSCustomObject]@{
+            DatastoreName = $datastore.Name
+            FreeSpaceGB = [Math]::Round($datastore.FreeSpaceGB, 2)
+            CapacityGB = [Math]::Round($datastore.CapacityGB, 2)
+        }
+    }
 }
 
 # Export the cluster data to the first sheet in the Excel file
 $excelPath = Join-Path -Path $reportsDir -ChildPath "VMware_Assessment.xlsx"
-$clusterData | Export-Excel -Path $excelPath -WorksheetName "Cluster Summary" -AutoSize -TableName "ClusterSummary"
+$clusterData | Export-Excel -Path $excelPath -WorksheetName "ClusterSummary" -AutoSize -TableName "ClusterSummary"
 
 # Export the host data to the second sheet in the Excel file
-$hostData | Export-Excel -Path $excelPath -WorksheetName "Host Summary" -AutoSize -TableName "HostSummary" -Append
+$hostData | Export-Excel -Path $excelPath -WorksheetName "HostSummary" -AutoSize -TableName "HostSummary" -Append
 
-##$results | Export-Excel -Path $excelPath -WorksheetName $worksheetName -AutoSize -TableName "hostservices"
+# Export the average data to the third sheet in the Excel file
+$averageData | Export-Excel -Path $excelPath -WorksheetName "AverageUsageSummary" -AutoSize -TableName "AverageUsageSummary" -Append
+
+# Export the VM data to the fourth sheet in the Excel file
+$vmData | Export-Excel -Path $excelPath -WorksheetName "PoweredOnVMs" -AutoSize -TableName "PoweredOnVMs" -Append
+
+# Export the datastore data to the fifth sheet in the Excel file
+$datastoreData | Export-Excel -Path $excelPath -WorksheetName "DatastoreSummary" -AutoSize -TableName "DatastoreSummary" -Append
